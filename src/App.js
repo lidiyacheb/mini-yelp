41267245034506534ee248f7961073a03cfb88df
lidiyacheb <lidia.saulite@gmail.com>
2020-11-20 19:09:29 +0100

    add home and detail page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import fakeData from './fakeData.js';
+import Card from "./Card.js"
+import {Switch, Route } from "react-router-dom";
+import DetailPage from "./DetailPage.js"
+
 
 function App() {
+  const [data, setData] = useState(fakeData);
+
+  useEffect(() => {
+     setData(data);
+     console.log(data);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+    <Switch>
+          <Route exact path="/">
+          <header className="header">
+    <form className="form-inline mx-5 my-2 my-lg-0">
+            <label>
+                <input
+                className="form-control ml-sm-5" placeholder="Type restaurant"
+                />
+                <input
+                className="form-control ml-sm-5" placeholder="Type location"/>
+            </label>
+            <input className="btn btn-danger ml-sm-3 my-2 my-sm-0" type="submit" value="Search" />
+        </form>
+    </header>
+    <Card data={data}/>
+          </Route>
+          <Route path="/:id">
+             <DetailPage data={data}/>
+          </Route>
+      </Switch>
+    </>    
   );
 }
 
---
+++ src/fakeData.js
@@ -0,0 +1,34 @@
+const fakeData = [
+    {
+    id:	1,
+	name: "Grill House BadaBing",
+	image: "https://images.unsplash.com/photo-1471253794676-0f039a6aae9d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+	city: {id: 1, name: "Berlin"},
+	adress: "ABC Straße 1, 10349",
+	rating: 4,
+	types: ["vegetarian", "vegan", "imbiss"],
+    comments: [{ id: "1" ,text: "comment1"  , date: ""}]
+},
+{
+    id:	2,
+	name: "Sushi Bar",
+	image: "https://images.unsplash.com/photo-1482275548304-a58859dc31b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80",
+	city: {id: 1, name: "Hamburg"},
+	adress: "ABC Straße 1, 10349",
+	rating: 5,
+	types: ["vegetarian", "vegan", "imbiss"],
+    comments: [{ id: "2" ,text: "comment1"  , date: ""}]
+},
+{
+    id:	3,
+	name: "Spanish Restaurant",
+	image: "https://images.unsplash.com/photo-1482275548304-a58859dc31b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80",
+	city: {id: 1, name: "Hamburg"},
+	adress: "ABC Straße 1, 10349",
+	rating: 5,
+	types: ["vegetarian", "vegan", "imbiss"],
+    comments: [{ id: "2" ,text: "comment1"  , date: ""}]
+}
+];
+
+export default fakeData;
